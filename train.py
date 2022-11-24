ff8a3bac0741f7fafe76d52e982ce63944ece4d3
Tom Saberhagen <tom@xform.llc>
2022-11-24 11:58:02 -0500

    Working not converg

--- train.py
+++ train.py
@@ -3,16 +3,23 @@ RL agent to play sentence simplification game
 
 Credits:
 Deep Reinforcement Learning Hands-On by Maxim Lapan provided a starting point
+Additional code and ideas adopted from cleanrl python project, dqn.py reference code
+---> https://github.com/vwxyzjn/cleanrl/blob/master/cleanrl/dqn.py
 
 """
 
 # ISSUES
-# Should consider iterating through full episodes
-#   & updating nets after a fixed number of episodes
-#   based on step rewards and final rewards of episode
+# Test wandb and video capture
+# Consider replacing replay buffer with stable baselines
+#   (may make better use of GPU)
 
 import argparse
+import os
+import sys
+import random
 import time
+from distutils.util import strtobool
+
 import gym
 import gyms
 import numpy as np
@@ -20,10 +27,11 @@ import pandas as pd
 import torch
 import torch.nn as nn
 import torch.optim as optim
-#from tensorboardX import SummaryWriter
-import rl_model
+from torch.utils.tensorboard import SummaryWriter
 import jsonlines
 import matplotlib.pyplot as plt
+
+import rl_model
 
 HYPERS = {
           "gamma":0.99,
@@ -32,6 +40,29 @@ HYPERS = {
           "epsilon_decay":-1e-4, # had -1e-4 earlier, sort of worked
           "solved_reward":10
 }
+
+def parse_args():
+  parser = argparse.ArgumentParser()
+  parser.add_argument("--env-id", type=str, default="gyms/TextWorld-v0",
+                      help="the id of the environment")
+  parser.add_argument("--exp-name", type=str, default=os.path.basename(__file__).rstrip(".py"),
+                      help="the name of this experiment")
+  parser.add_argument("--seed", type=int, default=1,
+                      help="seed of the experiment")
+  parser.add_argument("--torch-deterministic", type=lambda x: bool(strtobool(x)), default=True, nargs="?", const=True,
+                      help="if toggled, `torch.backends.cudnn.deterministic=False`")
+  parser.add_argument("--cuda", default=False,action="store_true")
+  parser.add_argument("--net", help="name of previously saved net to use")
+  parser.add_argument("--track", type=lambda x: bool(strtobool(x)), default=False, nargs="?", const=True,
+                      help="if toggled, this experiment will be tracked with Weights and Biases")
+  parser.add_argument("--wandb-project-name", type=str, default="text_simplification",
+                      help="the wandb's project name")
+  parser.add_argument("--wandb-entity", type=str, default=None,
+                      help="the entity (team) of wandb's project")
+  parser.add_argument("--capture-video", type=lambda x: bool(strtobool(x)), default=False, nargs="?", const=True,
+                      help="whether to capture videos of the agent performances (check out `videos` folder)")
+  args = parser.parse_args()
+  return args
 
 class Agent:
     """RL agent that interacts with environment"""
@@ -105,8 +136,8 @@ class Agent:
           d['end'] = self._display_env_info(info)
           print(f"Done_reward = {done_reward}.\n\n\n")
           d['reward']=done_reward
-          with jsonlines.open('logfile.jsonl',mode='a') as writer:
-            writer.write(d)
+#          with jsonlines.open('logfile.jsonl',mode='a') as writer:
+#            writer.write(d)
           self._reset()
           return done_reward
         else:
@@ -154,88 +185,132 @@ def batch_loss(batch, net, tgt_net, device="cpu"):
                             expected_state_action_values)
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--cuda", default=False,
-                        action="store_true")
-    parser.add_argument("--net")
-    args = parser.parse_args()
+    args = parse_args()
+    #Experiment tracking, following clearnrl reference 
+    run_name = f"{args.env_id}__{args.exp_name}__{args.seed}__{int(time.time())}"
+    if args.track:
+        import wandb
+        wandb.init(
+            project=args.wandb_project_name,
+            entity=args.wandb_entity,
+            sync_tensorboard=True,
+            config=vars(args),
+            name=run_name,
+            monitor_gym=True,
+            save_code=True,
+        )
+    writer = SummaryWriter(f"runs/{run_name}")
+#    writer.add_text(
+#        "hyperparameters",
+#        "|param|value|\n|-|-|\n%s" % ("\n".join([f"|{key}|{value}|" for key, value in HYPERS])),
+#    )
+
+    # set seeds as recommended in cleanrl
+    random.seed(args.seed)
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    torch.backends.cudnn.deterministic = args.torch_deterministic
+
+    #Set cuda flag
     device = torch.device("cuda" if args.cuda else "cpu")
-    env = gym.make("gyms/TextWorld-v0")
-    #Make new net with correct input size
+
+    #Set up the gym environment
+    env = gym.make(f"{args.env_id}")
+    env = gym.wrappers.RecordEpisodeStatistics(env)
+    if args.capture_video:
+      env = gym.wrappers.RecordVideo(env,f"videos/{run_name}")
+#    env.seed(args.seed)
+    env.action_space.seed(args.seed)
+    env.observation_space.seed(args.seed)
+
+    #Create a policy network and target network
+    # Input dimensions match observation space shape
+    # Output dimensions match action space shape
     obs_shape = env.observation_space.shape
     net = rl_model.RL(obs_shape,
                       env.action_space.n).to(device)
     tgt_net = rl_model.RL(obs_shape,
                       env.action_space.n).to(device)
+
     #by default start at epsilon = 1
     epsilon = 1.0
-    #if a saved network was specified, load it
+
+    #if a previously saved network was specified, then load it
     if args.net:
       net.load_state_dict(torch.load(args.net))
       net.eval()
       tgt_net.load_state_dict(torch.load(args.net))
       tgt_net.eval()
-      #also, take down epsilon to 0.5
+      #when using saved network, take down epsilon to 0.5
       epsilon = 0.5
-    #else: Should I be Xavier initializing my networks? 
-      #--> https://github.com/bentrevett/pytorch-rl/blob/master/1%20-%20Vanilla%20Policy%20Gradient%20(REINFORCE)%20%5BCartPole%5D.ipynb    
-    #Start tensorboard
-#    writer = SummaryWriter(comment="gyms/TextWorld-v0")
-    #Prepare for training loop
+
+    #otherwise .. should I be Xavier initializing my networks? 
+      #--> https://github.com/bentrevett/pytorch-rl/blob/master/1%20-%20Vanilla%20Policy%20Gradient%20(REINFORCE)%20%5BCartPole%5D.ipynb
+    
+    #create an agent who will act in the environment
     agent = Agent(env)
+    
+    #prepare for the training loop
     optimizer = optim.Adam(net.parameters(),lr=HYPERS['learning_rate'])
-    total_rewards = []
+    game_rewards = []
     move = 0
     ts_move = 0
     ts = time.time()
     best_m_reward = None
-    #Begin training loop
+    
+    #run the training loop
     while True: 
       move += 1
-      #Play a step and get reward
-      reward = agent.play_step(net, epsilon, device=device)
-      if reward is not None:  # if this is final step in episode
-        #Update rewards metrics
-        total_rewards.append(reward)
-        n_games = len(total_rewards)
-        m_reward = np.mean(total_rewards[-100:])
-        #Calculate game speed
+      #play one step and get reward
+      final_reward = agent.play_step(net, epsilon, device=device)
+      if final_reward is not None:  # if this is final step in episode
+        #update rewards metrics
+        game_rewards.append(final_reward)
+        num_games = len(game_rewards)
+        avg_game_reward = np.mean(game_rewards[-100:])
+        #calculate game speed
         game_time = (time.time() - ts)
         ts = time.time()
-        #Log progress
+        #log progress
         print("%d: moves %d games, avg game score (last 100) %.3f, "
               "epsilon %.2f, speed %.2f seconds/game" % (
-                move, n_games, m_reward, epsilon, game_time))
-#        writer.add_scalar("avg_score_100", m_reward, move)
-#        writer.add_scalar("game_score", reward, move)
+                move, num_games, avg_game_reward, epsilon, game_time))
+        writer.add_scalar("charts/final_reward", final_reward, move)
+        writer.add_scalar("charts/epslion",epsilon,move)
         #If at least 100 games done:  
-        if(n_games>100):
+        if(num_games>100):
+          writer.add_scalar("charts/100_game_average", avg_game_reward, move)
           #Check if average score (last 100 games) is a new record .. if so, save model
-          if (best_m_reward is None) or (m_reward > best_m_reward):
-            torch.save(net.state_dict(),"model_save/TextWorld-avgscore_%.0f.dat" % m_reward)
-            plt.plot(total_rewards, 'o-r')
-            plt.ylabel('Reward')
-            plt.savefig('TextWorld-avgscore_%.0f.pdf' % m_reward)
+          if (best_m_reward is None) or (avg_game_reward > best_m_reward):
+            torch.save(net.state_dict(),"model_save/TextWorld-avgscore_%.0f.dat" % avg_game_reward)
+#            plt.plot(game_rewards, 'o-r')
+#            plt.ylabel('Reward')
+#            plt.savefig('TextWorld-avgscore_%.0f.pdf' % avg_game_reward)
             if best_m_reward is not None: 
-              print("Best reward updated %.3f -> %.3f" % (best_m_reward, m_reward))
-            best_m_reward = m_reward
+              print("Best reward updated %.3f -> %.3f" % (best_m_reward, avg_game_reward))
+            best_m_reward = avg_game_reward
           #If average is high enough, report game solved
-          if m_reward > HYPERS['solved_reward']:
+          if avg_game_reward > HYPERS['solved_reward']:
             print("Solved in %d moves" % move)
-            break
-      #Now we are done with the "if" for final step
-      #For every step: 
-      #Update the target network each 128 moves 
+            env.close()
+            writer.close()
+            sys.exit(0)
+      #(now we are done with the "if" for 'final step')
+
+      #periodically sample batches and update network
+      if move % 32 == 0:
+        optimizer.zero_grad()
+        batch = agent._sample_buffer(HYPERS["batch_size"])
+        if len(batch)==HYPERS["batch_size"]:
+          loss_t = batch_loss(batch, net, tgt_net, device=device)
+          writer.add_scalar("losses/td_loss",loss_t.item(), move)
+          loss_t.backward()
+          optimizer.step()
+
+      #periodically update the target network 
       if move % 128 == 0: 
         tgt_net.load_state_dict(net.state_dict())
-      #Zero the gradients, get a batch, calculate loss, minimize loss
-      #Should I do this every 32 moves? 64 moves? 128 moves?
-      optimizer.zero_grad()
-      batch = agent._sample_buffer(HYPERS["batch_size"])
-      if len(batch)==HYPERS["batch_size"]:
-        loss_t = batch_loss(batch, net, tgt_net, device=device)
-        loss_t.backward()
-        optimizer.step()
-      #update epslion
+
+      #decrease epslion
       epsilon += HYPERS['epsilon_decay']
 
